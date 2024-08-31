6c7253243cf02e3716dc4d7c8addbc28df164829
harshjhaSparrow <harsh.j@sparrowrms.in>
2024-08-31 12:30:29 +0530

    path matching done

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -22,13 +22,13 @@ export const Route = createRootRoute({
         <Link to="/status" className="[&.active]:font-bold">
           Status
         </Link>
-        <Link to="/user" className="[&.active]:font-bold">
+        <Link to="/user" activeOptions={{ exact: true }} className="[&.active]:font-bold">
           User
         </Link>
-        <Link to="/user/123" className="[&.active]:font-bold">
+        <Link to="/user/123" activeOptions={{ exact: true }} className="[&.active]:font-bold">
           User/12
         </Link>
-        <Link to="/user/123/edit" className="[&.active]:font-bold">
+        <Link to="/user/123/edit" activeOptions={{ exact: true }} className="[&.active]:font-bold">
           User/123/edit
         </Link>
         <Link to="/ChildOfLayout" className="[&.active]:font-bold">
@@ -40,13 +40,13 @@ export const Route = createRootRoute({
         <Link to="/ChildOfLayout3" className="[&.active]:font-bold">
           ChildOfLayout3
         </Link>
-        <Link to="/settings" className="[&.active]:font-bold">
+        <Link to="/settings" activeOptions={{ exact: true }} className="[&.active]:font-bold">
           Settings
         </Link>
-        <Link to="/settings/notifications" className="[&.active]:font-bold">
+        <Link to="/settings/notifications" activeOptions={{ exact: true }} className="[&.active]:font-bold">
           notifications
         </Link>
-        <Link to="/settings/profile" className="[&.active]:font-bold">
+        <Link to="/settings/profile" activeOptions={{ exact: true }} className="[&.active]:font-bold">
           profile
         </Link>
       </div>
