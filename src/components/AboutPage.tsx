f5e344eace4aa9dea31bece1da6a0254bfccd22b
harshjhaSparrow <harsh.j@sparrowrms.in>
2024-08-31 13:58:12 +0530

    Updated Code

--- src/components/AboutPage.tsx
+++ src/components/AboutPage.tsx
@@ -1,42 +1,50 @@
 import React from "react";
-import { Tabs, TabList, TabPanels, Tab, TabPanel, VisuallyHidden, Box } from "@chakra-ui/react";
-
+import {
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+  VisuallyHidden,
+  Box,
+} from "@chakra-ui/react";
 
 const AboutPage: React.FC = () => {
   return (
     <>
-     <div className="p-2">
-      <Tabs variant="enclosed">
-      <TabList>
-        <Tab>Tab 1</Tab>
-        <Tab>Tab 2</Tab>
-        <Tab>Tab 3</Tab>
-      </TabList>
+      <div className="p-2">
+        <Tabs variant="enclosed">
+          <TabList>
+            <Tab>Tab 1</Tab>
+            <Tab>Tab 2</Tab>
+            <Tab>Tab 3</Tab>
+          </TabList>
 
-      <TabPanels>
-        <TabPanel>
-          <p>Content for Tab 1</p>
-        </TabPanel>
-        <TabPanel>
-          <p>Content for Tab 2</p>
-        </TabPanel>
-        <TabPanel>
-          <p>Content for Tab 3</p>
-        </TabPanel>
-      </TabPanels>
-    </Tabs>
-    </div>
-    <div>
-       <Box>
-      <h1>Visible Heading</h1>
-      <VisuallyHidden>
-        <p>This text is hidden from view but accessible to screen readers.</p>
-      </VisuallyHidden>
-      <p>Some other visible content.</p>
-    </Box>
-    </div>
+          <TabPanels>
+            <TabPanel>
+              <p>Content for Tab 1</p>
+            </TabPanel>
+            <TabPanel>
+              <p>Content for Tab 2</p>
+            </TabPanel>
+            <TabPanel>
+              <p>Content for Tab 3</p>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </div>
+      <div>
+        <Box>
+          <h1>Visible Heading</h1>
+          <VisuallyHidden>
+            <p>
+              This text is hidden from view but accessible to screen readers.
+            </p>
+          </VisuallyHidden>
+          <p>Some other visible content.</p>
+        </Box>
+      </div>
     </>
-   
   );
 };
 
