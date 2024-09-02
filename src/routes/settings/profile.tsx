66ad2a2d3d24cf5b308fabb68faab7ab3f0bc7bd
harshjhaSparrow <harsh.j@sparrowrms.in>
2024-09-02 15:23:45 +0530

    Charts

--- src/routes/settings/profile.tsx
+++ src/routes/settings/profile.tsx
@@ -6,21 +6,22 @@ import HeatmapChart from "../../components/HeatMapChart";
 
 export const Route = createFileRoute("/settings/profile")({
   component: () => (
-    <div className="flex flex-wrap gap-0 overflow-auto h-[90vh] p-4">
-      {/* Each item will adjust its width based on screen size */}
-      <div className="w-full sm:w-1/2 lg:w-1/3 p-2 flex flex-col rounded-lg border shadow-md">
-        <div className="text-lg font-semibold mb-2">Heading here</div>
-        <Donut />
-      </div>
-      <div className="w-full sm:w-1/2 lg:w-1/3 p-2 flex flex-col rounded-lg border shadow-md">
-        <BoxPlotChart />
-      </div>
-      <div className="w-full sm:w-1/2 lg:w-1/3 p-2 flex flex-col rounded-lg border shadow-md">
-        <ChartNotification />
-      </div>
-      <div className="w-full sm:w-1/2 lg:w-1/3 p-2 flex flex-col rounded-lg border shadow-md">
-        <HeatmapChart />
-      </div>
-    </div>
+<div className="flex flex-wrap gap-1 overflow-auto p-4">
+  {/* Each item will adjust its width based on screen size */}
+  <div className="w-full sm:w-1/2 lg:w-1/3 p-2 flex flex-col rounded-lg border shadow-md">
+    <div className="text-lg font-semibold mb-2">Heading here</div>
+    <Donut />
+  </div>
+  <div className="w-full sm:w-1/2 lg:w-1/3 p-2 flex flex-col rounded-lg border shadow-md">
+    <BoxPlotChart />
+  </div>
+  <div className="w-full sm:w-1/2 lg:w-1/3 p-2 flex flex-col rounded-lg border shadow-md">
+    <ChartNotification />
+  </div>
+  <div className="w-full sm:w-1/2 lg:w-1/3 p-2 flex flex-col rounded-lg border shadow-md">
+    <HeatmapChart />
+  </div>
+</div>
+
   ),
 });
