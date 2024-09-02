92dd2f584a2c2fffc42fa1b0536de88b4ed907c4
harshjhaSparrow <harsh.j@sparrowrms.in>
2024-09-02 09:22:06 +0530

    Updated Code

--- src/components/Donut.tsx
+++ src/components/Donut.tsx
@@ -1,15 +1,15 @@
-import React, { useState } from 'react';
-import Chart from 'react-apexcharts';
+import React, { useState } from "react";
+import Chart from "react-apexcharts";
 
 const Donut: React.FC = () => {
   // Setting up state for options, series, and labels
   const [options] = useState({});
-  const [series] = useState([44, 55, 41, 17, 15]);
-  const [labels] = useState(['A', 'B', 'C', 'D', 'E']);
+  const [series] = useState([360, 55, 41, 17, 15]);
+  const [labels] = useState(["A", "B", "C", "D", "E"]);
 
   return (
     <div className="donut">
-      <Chart 
+      <Chart
         options={{ ...options, labels }} // Adding labels to options
         series={series}
         type="donut"
@@ -17,6 +17,6 @@ const Donut: React.FC = () => {
       />
     </div>
   );
-}
+};
 
 export default Donut;
