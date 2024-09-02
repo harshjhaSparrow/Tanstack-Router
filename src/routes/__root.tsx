3b90f73009ea6cde22446047e5846f63c0abf03c
harshjhaSparrow <harsh.j@sparrowrms.in>
2024-09-02 13:15:04 +0530

    Made Side layout using tailwind

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -11,15 +11,21 @@ export const Route = createRootRoute({
         <div className="flex ">
           <Sidebar />
           <div className="flex-1 flex flex-col w-full h-[100vh] justify-between items-center p-4 bg-gray-100">
-            <div className="h-[10vh] font-semibold text-2xl w-full">
-              <div className="w-full">{pageTitle}</div>
+            <div className="h-[10vh] my-3 flex justify-between font-bold text-2xl w-full">
+              <div className="center">{pageTitle}</div>
+              <div className="center">
+                <img
+                  className="w-10 cursor-pointer h-10 p-1 rounded-full ring-2 ring-gray-300 dark:ring-gray-500"
+                  src="https://picsum.photos/20"
+                  alt="Bordered avatar"
+                />
+              </div>
             </div>
             <div className="h-[90vh] w-full">
               <Outlet />
             </div>
           </div>
         </div>
-
         <TanStackRouterDevtools />
       </>
     );
